ec33362c58c586bf2bd51f0d3f097c84b6ec2b8a
Alzorath57 <julien.holtzinger@gmail.com>
2026-05-16 21:28:09 +0200

    feat: ajout des filtres de catégories

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,12 +1,21 @@
+// Récupère les travaux depuis l'API
+
 async function getWorks() {
   const response = await fetch("http://localhost:5678/api/works");
   const works = await response.json();
   return works;
 }
+
+// ========== INITIALISATION ==========
+
 async function init() {
   const works = await getWorks();
   displayWorks(works);
+  const categories = await getCategories();
+  displayCategories(categories);
 }
+
+// Affiche les travaux dans la galerie
 
 function displayWorks(works) {
   const gallery = document.querySelector(".gallery");
@@ -21,5 +30,22 @@ function displayWorks(works) {
     figure.appendChild(figcaption);
     gallery.appendChild(figure);
   });
+}
+
+// Récupère les catégories depuis l'API
+
+async function getCategories() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const categories = await response.json();
+  return categories;
+}
+
+// Affiche les catégories dans le filtre
+
+function displayCategories(categories) {
+  const filters = document.getElementById("filters");
+  const allBouton = document.createElement("button");
+  allBouton.textContent = "Tous";
+  filters.appendChild(allBouton);
 }
 init();
